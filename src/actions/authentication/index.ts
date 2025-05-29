e6a30dad2b0bc5a4b847dc6a99a3e0ab4b00d9a6
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2025-05-29 14:18:00 -0300

    feat: add teacher form

---
+++ src/actions/authentication/index.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+import { actionClient } from "@/lib/safe-action";
+
+import { authenticationSchema } from "./schema";
+
+export const authenticate = actionClient
+  .schema(authenticationSchema)
+  .action(async ({ parsedInput }) => {
+    const response = await fetch("http://localhost:8080/api/auth/login", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(parsedInput),
+    });
+
+    if (response.status === 400) {
+      throw new Error("Usuário não encontrado!");
+    }
+
+    const data = await response.json();
+
+    const cookieStore = await cookies();
+
+    cookieStore.set("token", data.token, {
+      httpOnly: true,
+      secure: true,
+    });
+
+    cookieStore.set("id", data.user.id, {
+      httpOnly: true,
+      secure: true,
+    });
+
+    cookieStore.set("name", data.user.name, {
+      httpOnly: true,
+      secure: true,
+    });
+  });
