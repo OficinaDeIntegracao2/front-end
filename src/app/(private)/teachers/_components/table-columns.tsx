0a3bc6f67a009f2ba2cafb7f8b70b4d306df0d20
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2025-06-01 17:10:18 -0300

    feat: add table teachers

---
+++ src/app/(private)/teachers/_components/table-columns.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+import { DateTime } from "luxon";
+
+import { Button } from "@/components/ui/button";
+
+export type Teacher = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<Teacher>[] = [
+  {
+    accessorKey: "createdAt",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Data de criação
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return DateTime.fromISO(row.original.createdAt, {
+        zone: "UTC",
+      }).toLocaleString();
+    },
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Nome
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+];
